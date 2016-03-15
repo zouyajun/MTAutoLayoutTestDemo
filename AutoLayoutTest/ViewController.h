92e58d41f7b1fdea0f8673d1c912fe87d98c09a3
@zouyajun <@zouyajunzyj@sina.com>
2016-03-15 15:30:04 +0800

    first commit

---
+++ AutoLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutTest
+//
+//  Created by Ryan on 16/1/8.
+//  Copyright © 2016年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutTest
+//
+//  Created by Ryan on 16/1/8.
+//  Copyright © 2016年 Ryan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
